4e91033bc8e724a6d09c99ed9ccac6c1b166dc90
Devanshi Karthik <dkarthik@microsoft.com>
2022-08-25 17:21:55 +0530

    api's integrated

--- Client/src/Components/ProfileCard.js
+++ Client/src/Components/ProfileCard.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import "../App.css"
+import React from "react";
+import "../App.css";
 
-function ProfileCard (props){
-
-    return (
-        <div className="profileCard">
-            {console.log(props)}
-            <h2> {props.props.first_name} </h2>
-            <h3> {props.props.city}</h3>
-            <h3> {props.props.state}</h3>
-            <h3> {props.props.postcode}</h3>
-            
-
-        </div>
-    );
+function ProfileCard(props) {
+  return (
+    <div className="profileCard">
+      <h2> {props.props.username} </h2>
+    </div>
+  );
 }
 
 export default ProfileCard;
